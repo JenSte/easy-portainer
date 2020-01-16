f695f888cff1d5c60b08b0d93ed3d82db819abb8
Jens Steinhauser <jens.steinhauser@gmail.com>
2020-01-16 20:36:35 +0000

    Enable support for Watchtower

--- start.sh
+++ start.sh
@@ -16,12 +16,17 @@ UI_CREDENTIALS="${UI_USER}:${UI_CREDENTIALS}"
 REGISTRY_CREDENTIALS="$(docker run --rm httpd:2.4-alpine htpasswd -nbB admin ${REGISTRY_PASS} | cut -d ":" -f 2)"
 REGISTRY_CREDENTIALS="${REGISTRY_USER}:${REGISTRY_CREDENTIALS}"
 
+# Set the label to update the container via Watchtower (https://hub.docker.com/r/containrrr/watchtower).
+# (Watchtower itself has to be started separately.)
+WATCHTOWER_ENABLE=true
+
 export PORTAINER_CREDENTIALS
 export DASHBOARD_CREDENTIALS
 export UI_CREDENTIALS
 export REGISTRY_CREDENTIALS
 export EMAIL
 export DOMAIN
+export WATCHTOWER_ENABLE
 
 docker-compose build
 docker-compose up -d
